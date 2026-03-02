c893cefae713b910bcc52451f3ec830fd73f4f5b
Marlyn Grullon <marlyngrullon70@gmail.com>
2026-03-02 16:15:12 -0500

    Updated mode selection.

--- script.js
+++ script.js
@@ -1,4 +1,18 @@
+// Dropdown toggle function
 function toggleDropdown() {
     const menu = document.getElementById("modeDropdown");
     menu.classList.toggle("show");
 }
+
+// handle selection between modes
+document.addEventListener("DOMContentLoaded", () => {
+    const cards = document.querySelectorAll(".mode-card");
+
+    cards.forEach(card => {
+        card.addEventListener("click", () => {
+            cards.forEach(c => c.classList.remove("active"));
+
+            card.classList.add("active");
+        });
+    });
+});
